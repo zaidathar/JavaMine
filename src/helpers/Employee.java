7c5d59d433065dcc40e538b81c7f20eff470472c
zaidathar <codewithzukon@gmail.com>
2023-02-13 13:46:08 +0530

    Helper class added
    Need helper class for problems
    -Instead of creating at class level
    -Better to create at project level and use it

---
+++ src/helpers/Employee.java
@@ -0,0 +1,61 @@
+package helpers;
+
+public class Employee {
+    private String name;
+    private String department;
+    private Double salary;
+    private String contact;
+    private String email;
+
+    Employee(){
+
+    }
+
+    public Employee(String name, String department, Double salary, String contact, String email) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.contact = contact;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
