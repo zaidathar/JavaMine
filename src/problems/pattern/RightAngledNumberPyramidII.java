f1af15640f295df664ad16a6077e910737a76380
zaidathar <codewithzukon@gmail.com>
2023-04-30 11:13:21 +0530

    Rightangled Number Pyramid II

--- src/problems/pattern/RightAngledNumberPyramidII.java
+++ src/problems/pattern/RightAngledNumberPyramidII.java
@@ -2,14 +2,14 @@ package problems.pattern;
 
 import java.util.Scanner;
 
-public class RightangledNumberPramid {
+public class RightAngledNumberPyramidII {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
 
         for(int i =1 ;i<=row;i++){
             for(int j= 1 ;j<=i;j++){
-                System.out.print(j);
+                System.out.print(i);
             }
             System.out.println();
         }
