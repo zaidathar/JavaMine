9e0048cd36d06535d6a8ac027af23344697354b5
zaidathar <codewithzukon@gmail.com>
2023-04-30 11:02:25 +0530

    Rightangled Triangle Star Pattern

---
+++ src/problems/pattern/RightAngledTriangleStarPattern.java
@@ -0,0 +1,18 @@
+package problems.pattern;
+
+import java.util.Scanner;
+
+public class RightAngledTriangleStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for(int i =1 ;i<=row;i++){
+            for(int j= 0 ;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
