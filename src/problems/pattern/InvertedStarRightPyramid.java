f8716abdea28730c7536a2d81e982ccf7fb98774
zaidathar <codewithzukon@gmail.com>
2023-05-02 12:30:53 +0530

    Inverted Star Right Pyramid Pattern

---
+++ src/problems/pattern/InvertedStarRightPyramid.java
@@ -0,0 +1,17 @@
+package problems.pattern;
+
+import java.util.Scanner;
+
+public class InvertedStarRightPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = n ; i>0 ;i--){
+            for(int j = 0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
