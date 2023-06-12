85aa092fc9146bc746cad6cba7df479794f369e8
zaidathar <codewithzukon@gmail.com>
2023-06-12 14:20:46 +0530

    Builder for class

---
+++ src/builder/BuilderTest.java
@@ -0,0 +1,12 @@
+package builder;
+
+public class BuilderTest {
+    public static void main(String[] args) {
+        MyClassWithBuilder builder = MyClassWithBuilder.builder()
+                                        .name("Java")
+                                        .address("Internet")
+                                        .age(25)
+                                        .build();
+        System.out.println(builder);
+    }
+}
---
+++ src/builder/MyClassWithBuilder.java
@@ -0,0 +1,57 @@
+package builder;
+
+public class MyClassWithBuilder {
+    private String name;
+    private int age;
+    private String address;
+
+    public MyClassWithBuilder(CustomBuilder builder){
+        this.name = builder.name;
+        this.address = builder.address;
+        this.age = builder.age;
+    }
+
+    public static CustomBuilder builder(){
+        return new CustomBuilder();
+    }
+
+    @Override
+    public String toString(){
+        return "Name : "+this.name+"\n"+
+                "Age : "+this.age+"\n"+
+                "Address: "+this.address+"\n";
+    }
+
+    public static class CustomBuilder{
+        private String name;
+        private int age;
+        private String address;
+
+        public CustomBuilder(){}
+        public CustomBuilder(String name, int age, String address){
+            this.name = name;
+            this.age = age;
+            this.address = address;;
+        }
+
+        public CustomBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        public CustomBuilder age(int age){
+            this.age = age;
+            return this;
+        }
+
+        public CustomBuilder address(String address){
+            this.address = address;
+            return this;
+        }
+
+        public MyClassWithBuilder build(){
+            return new MyClassWithBuilder(this);
+        }
+
+    }
+}
