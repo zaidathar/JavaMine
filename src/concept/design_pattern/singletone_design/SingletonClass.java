6ad435f31e7e8dc62d7d601e607a3b2f1fa36f2f
zaidathar <codewithzukon@gmail.com>
2023-06-12 14:12:49 +0530

    Singleton class with dual check

---
+++ src/concept/design_pattern/singletone_design/SingletonClass.java
@@ -0,0 +1,23 @@
+package concept.design_pattern.singletone_design;
+
+public class SingletonClass{
+    // This is doubly check singleton class 
+    private static volatile SingletonClass _instance;
+
+    private SingletonClass(){
+    }
+
+    public static SingletonClass getInstance(){
+        //Dual lock check for thread safety 
+        if( _instance == null){
+            synchronized(SingletonDemo.class){
+                if(_instance == null){
+                    _instance = new SingletonClass();
+                }
+            }
+        }   
+        return _instance;
+    }
+
+    
+}
