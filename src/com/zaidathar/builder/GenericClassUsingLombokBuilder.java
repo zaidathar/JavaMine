5ebf0496e0362588dae0527edd049014fb560fff
zaidathar <codewithzukon@gmail.com>
2023-07-20 14:21:54 +0530

    Builder commit

--- src/com/zaidathar/builder/GenericClassUsingLombokBuilder.java
+++ src/com/zaidathar/builder/GenericClassUsingLombokBuilder.java
@@ -1,8 +1,8 @@
 package com.zaidathar.builder;
 
 // This will be available only project lombok jar is present
-@Builder
-@Data
+//@Builder
+//@Data
 public class GenericClassUsingLombokBuilder<T,G>{
     private T value;
     private G alternate;
