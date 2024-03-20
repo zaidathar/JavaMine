9c82e357576215c632f922abf7686454e866caec
zaidathar <codewithzukon@gmail.com>
2024-03-20 22:03:23 +0530

    Find First element using stream

--- src/com/zaidathar/problems/streams/Basics.java
+++ src/com/zaidathar/problems/streams/Basics.java
@@ -17,8 +17,32 @@ public class Basics {
         Set<Integer> set = new HashSet<>();
         return ls.stream().anyMatch( a -> !set.add(a));
     }
+
+    public static <E> E findFirstUsingOptional(Collection<E> collection) throws Exception {
+         Optional<E> firstValue = collection.stream().findFirst();
+         if(firstValue.isPresent())
+             return firstValue.get();
+         else
+             throw new Exception("Null value found ");
+    }
+
+
+    public static <E> E findFirst2(Collection<E> collection) throws Exception{
+        return collection.stream().reduce((first,second) -> first).orElse(null);
+    }
     public static void main(String[] args) {
         List<Integer> intergerList = Arrays.asList(1,2,3,4,5,6,7,8,9);
         System.out.println("Duplicate found in list "+checkDuplicateUsingHashSet(intergerList));
+        List<Integer> nullList = null;
+        try{
+            System.out.println("First element in list is "+findFirst2(intergerList));
+            System.out.println("First element in null list is "+findFirst2(nullList));
+        }
+        catch (Exception e){
+            System.out.println("Exception caught : "+e.getMessage());
+        }
+
     }
+
+
 }
