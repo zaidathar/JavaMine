8ec7a190498530e41334c6ea78512c99e4f75bcd
zaidathar <codewithzukon@gmail.com>
2024-03-25 06:35:59 +0530

    Diamond problem commit

--- src/com/zaidathar/concept/inheritence/diamond_problem/DiamondProblemAbstractClass.java
+++ src/com/zaidathar/concept/inheritence/diamond_problem/DiamondProblemAbstractClass.java
@@ -3,13 +3,18 @@ package com.zaidathar.concept.inheritence.diamond_problem;
 public abstract class DiamondProblemAbstractClass implements DiamondProblemInterface1, DiamondProblemInterface2{
 
     /**
-     * If this abstract class is empty it will not give error to implement interface method
-     * as we will be implementing those abstract methods in those classes who is extending this class.
+
+      If this abstract class is empty, it will not give error to implemented interface's method
+      as we will be implementing those abstract methods in those classes who are extending this class.
+
      */
 
+
     /**
-     * If we make abstract method with same name as Interface method it will not impact on abstract
-     * but class need to implement this method who is extending this abstract class.
+
+      If we make abstract method with same name as Interface method it will not impact on abstract
+      but class need to implement this method who is extending this abstract class.
+
      */
     //    public abstract void printMyName();
 
--- src/com/zaidathar/concept/inheritence/diamond_problem/DiamondProblemClassImpl.java
+++ src/com/zaidathar/concept/inheritence/diamond_problem/DiamondProblemClassImpl.java
@@ -2,14 +2,16 @@ package com.zaidathar.concept.inheritence.diamond_problem;
 
 public class DiamondProblemClassImpl extends DiamondProblemAbstractClass implements DiamondProblemInterface1, DiamondProblemInterface2{
     /**
-     * No need to implement printMyName method as it already implemented in out abstract class
-     * If we implements both interface in this class again we are getting error for implementing abstract method.
-     * as this class is child of abstract class that already implemented method of both interface
-     * at the end printMyName came from interface so no need to again implement in this class.
+
+      No need to implement printMyName method as it already implemented in abstract class.
+      If we implemented both interfaces in this class again we are getting error for implementing abstract method.
+      as this class is child of abstract class that already implemented method of both interface
+      at the end printMyName came from interface so no need to again implement in this class.
+
      */
 
     public static void main(String[] args) {
         DiamondProblemClassImpl obj = new DiamondProblemClassImpl();
-        obj.printMyName(); // this implementation came from abstract class.
+        obj.printMyName(); // this implementation will come from abstract class.
     }
 }
