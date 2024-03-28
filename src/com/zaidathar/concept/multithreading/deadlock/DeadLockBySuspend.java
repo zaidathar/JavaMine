0da51aa547c210d40ce916c198d2a6141c8f1d47
zaidathar <codewithzukon@gmail.com>
2024-03-28 23:56:40 +0530

    Deadlock by suspend method

--- src/com/zaidathar/concept/multithreading/deadlock/DeadLockBySuspend.java
+++ src/com/zaidathar/concept/multithreading/deadlock/DeadLockBySuspend.java
@@ -24,10 +24,39 @@ public class DeadLockBySuspend {
                 "T2"
         );
 
+        // Daemon thread , when all user thread finished their task it will automatically close
+        th1.setDaemon(true);
+
+        // starting threads
         th1.start();
-        th2.start();
-        Thread.sleep(3000);
-        th1.suspend();
+
+        /**
+         // As only main thread is running now
+         //th2.start();
+
+         // Thread T1 will stop immediately as no user thread remains working
+         // If T2 is waiting for T1 to finish its task , despite closing of Main-Thread T1 will run
+         // and execute all logic as 1 user thread is active and running i.e. T2
+
+         */
+
+        /**
+            // Main Thread will wait for th1 and th2 to finish their tasks.
+            th1.join();
+            th2.join();
+         */
+
+
+        /**
+            // suspend and resume is deprecated as it doesn't release monitor lock
+            Thread.sleep(3000);
+            th1.suspend();
+
+            Thread.sleep(2000);
+            th1.resume();
+
+         */
+
         System.out.println("Exiting main thread");
     }
 }
