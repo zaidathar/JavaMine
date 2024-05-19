217811f063297e095a58f6705a1bb4f57d572fb7
zaidathar <codewithzukon@gmail.com>
2024-05-19 16:03:38 +0530

    Random number string generator

--- src/com/zaidathar/utils/StringUtils.java
+++ src/com/zaidathar/utils/StringUtils.java
@@ -13,5 +13,16 @@ public class StringUtils {
         }
         return sb.toString();
     }
+
+    public static String generateNumberString(int length){
+        StringBuilder sb = new StringBuilder();
+        String numbers = "0123456789";
+        SecureRandom RANDOM = new SecureRandom();
+        for (int i=0; i< length; i++) {
+            sb.append(numbers.charAt(RANDOM.nextInt(numbers.length())));
+        }
+        return sb.toString();
+
+    }
     
 }
