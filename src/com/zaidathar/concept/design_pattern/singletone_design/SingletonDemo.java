96dc53ce5ed76f0b4353c8921e82bf33d68b7293
zaidathar <codewithzukon@gmail.com>
2024-05-23 12:50:46 +0530

    SingletonDemo with Bil pugh

--- src/com/zaidathar/concept/design_pattern/singletone_design/SingletonDemo.java
+++ src/com/zaidathar/concept/design_pattern/singletone_design/SingletonDemo.java
@@ -22,5 +22,30 @@ public class SingletonDemo implements Runnable {
         t1.start();
         t2.start();
 
+        Thread t3 = new Thread(() ->{
+            System.out.println("Inside run method for "+Thread.currentThread().getName() +" to access bil pugh singleton object");
+            try{
+                SingletonByBilPugh singleton = SingletonByBilPugh.getInstance();
+                System.out.println("Hashcode "+singleton.hashCode());
+            }
+            catch (Exception e){
+                System.out.println("Exception caught for "+Thread.currentThread().getName()+" : "+e.getMessage());
+            }
+        },"t3");
+
+        Thread t4 = new Thread(() ->{
+            System.out.println("Inside run method for "+Thread.currentThread().getName() +" to access bil pugh singleton object");
+            try{
+                SingletonByBilPugh singleton = SingletonByBilPugh.getInstance();
+                System.out.println("Hashcode "+singleton.hashCode());
+            }
+            catch (Exception e){
+                System.out.println("Exception caught for "+Thread.currentThread().getName()+" : "+e.getMessage());
+            }
+        },"t4");
+
+        t3.start();
+        t4.start();
+
     }
 }
